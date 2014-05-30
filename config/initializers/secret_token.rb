c7ce7a38397deedfd6b38fa15d3a5c0d6b854dcd
Pravin Mhatre <sodel@Pravin-ubuntu.(none)>
2014-05-30 20:46:28 +0530

    Adding secrete token

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Holachef::Application.config.secret_token = 'd04cf269ad40b8cb472a753f05f1e6316d4517eecf97c568a43b0f865e7c9db038982fd9e4d24ae13c17dbb1bd22e2a9896c0ecc6c8e6106aa5b153c79120f4d'
