0a71a2484920573c761a30cc0f307f0c35652012
supriyak <supriyayatam@gmail.com>
2014-06-03 16:39:37 +0530

    Updated Css and Modal dynamic popup changes

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -17,8 +17,74 @@ function onComplete()
 //  each item inside modal script
      $(document).ready(function(){
         $('.item').click(function(){
+          $("#selected_item").val($(this).attr('id'));
+          $("#order-count").html("0")
           $('.modal-body').html($(this).html());
         });
+
+//      Increment order count
+        var cnt = 0;
+        $('#add-order-plus').unbind("click").click(function(e){
+            e.preventDefault();
+            $("#add-order-minus").removeClass("disabled");
+            $("#add-order-minus").removeAttr("disabled")
+            var selected_item = $("#selected_item").val()
+            var max_qty = $("#"+selected_item+" .menu_max_qty").val();
+            if (cnt == max_qty){
+                $("#add-order-plus").attr("disabled");
+                $("#add-order-plus").addClass("disabled");
+            }
+            if (cnt < parseInt(max_qty,10)){
+                cnt = cnt + 1;
+                $("#order-count").html(cnt);
+            }
+
+        })
+
+//      Decrement Order Count
+        $('#add-order-minus').unbind("click").click(function(e){
+            $("#add-order-plus").removeAttr("disabled");
+            $("#add-order-plus").removeClass("disabled");
+             e.preventDefault();
+             var decr = $("#order-count").html()
+             decr = parseInt(decr,10)
+             if (decr == 0){
+                 $("#add-order-minus").attr("disabled");
+                 $("#add-order-minus").addClass("disabled");
+             }
+             if(decr >=  1){
+                cnt = decr - 1;
+                $("#order-count").html(cnt);
+             }
+        })
+
+//      On PopUp Close
+        $("#order-done").unbind("click").click(function(e){
+            cnt = 0
+            e.preventDefault();
+            var selected_item = $("#selected_item").val()
+            var selected_id = "#"+selected_item+" li.recipe-stock"
+            $(selected_id).addClass('text-orange')
+            var order_count = parseInt($("#order-count").html(),10)
+
+//          Printing Cart message
+            if(order_count != 0){
+                $(selected_id).html(order_count +" in cart")
+                $("#"+selected_item+" li div.append-cart").append("<div class='add-cart'></div>")
+            }
+
+//          Calculation of Total Price
+            var total = 0;
+            $("#"+selected_item+" .menu_qty").val(order_count)
+            var menu_price = parseInt($("#"+selected_item+" .menu_price").val(), 10);
+            total = parseInt($("#total_price").val(),10) + (menu_price * order_count);
+
+            $("#total_price").val(total);
+            $("#bill_amount a").html(total+"/-");
+
+
+        })
+
      });
 
 // random splash on refresh
