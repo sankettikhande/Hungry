096d179052dc342ff145d7058151463cbfecc89b
Animesh <animeshjaain@gmail.com>
2014-06-12 18:54:37 +0530

    nested attribute ingredient.

--- app/controllers/cms/dishes_controller.rb
+++ app/controllers/cms/dishes_controller.rb
@@ -1,2 +1,16 @@
 class Cms::DishesController < Cms::ContentBlockController
+  def new
+    @dish = Dish.new
+    1.times { @dish.ingredients.build }
+    respond_to do |format|
+      format.html
+    end
+  end
+
+  def edit
+    @dish  = Dish.find(params[:id])
+    respond_to do |format|
+      format.html
+    end
+  end
 end
