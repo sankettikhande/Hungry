87fa2cfbe50759f259de4e7741c3a8c1f9673f90
supriyak <supriyayatam@gmail.com>
2014-06-30 18:51:48 +0530

    Removed global initializer

--- config/initializers/global.rb
+++
@@ -1,8 +0,0 @@
-$QA_DOMAIN_URL="http://qa.holachef.com"
-$gateway 	= 'sandbox'     # options - sandbox, staging or production
-$pmturl 	= 'h5omh3rcch'	# as per citrus pay merchant setup
-$accesskey	= '81HWEZWD41VN6NHDNNGB' # as per citrus pay merchant setup
-$apikey		= '573f85c0b61fdc202e3cff60b1c8333b5736a2e9' # as per citrus pay merchant setup
-
-$action 	= 'http://localhost:3000/home/index_call' # change it as required by your app...
-$returnUrl 	= 'http://localhost:3000/orders/callback'
