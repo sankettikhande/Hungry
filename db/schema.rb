ea959f953872caac07b799c5a31c0ce145d38794
Pravin Mhatre <pravinhmhatre@gmail.com>
2014-07-30 12:25:07 +0530

    Deleted tag.rb and tegging.rb in app root

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140618065157) do
+ActiveRecord::Schema.define(:version => 20140728110341) do
 
   create_table "categories", :force => true do |t|
     t.integer  "category_type_id"
@@ -25,6 +25,59 @@ ActiveRecord::Schema.define(:version => 20140618065157) do
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "chef_coordinates", :force => true do |t|
+    t.integer  "cheff_id"
+    t.string   "name"
+    t.text     "address_line_1"
+    t.text     "address_line_2"
+    t.string   "locality"
+    t.string   "landmark"
+    t.string   "city"
+    t.integer  "zip"
+    t.float    "longitude"
+    t.float    "latitude"
+    t.string   "contact_mobile"
+    t.string   "contact_landline"
+    t.string   "contact_email"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
+  end
+
+  create_table "chef_profiles", :force => true do |t|
+    t.integer  "cheff_id"
+    t.text     "about_me"
+    t.text     "why_i_love_cooking"
+    t.text     "people_places_that_inspire_my_food"
+    t.text     "best_compliment"
+    t.text     "why_do_i_want_to_join_hola"
+    t.string   "facebook_handle"
+    t.string   "twitter_handle"
+    t.text     "banners"
+    t.text     "brand_represented"
+    t.text     "brand_info"
+    t.text     "brand_banner"
+    t.datetime "created_at",                         :null => false
+    t.datetime "updated_at",                         :null => false
+    t.string   "brand_logo_file_name"
+    t.string   "brand_logo_content_type"
+    t.integer  "brand_logo_file_size"
+    t.datetime "brand_logo_updated_at"
+  end
+
+  create_table "cheff_cuisine_geographies", :force => true do |t|
+    t.integer  "cuisine_geography_id"
+    t.integer  "cheff_id"
+    t.datetime "created_at",           :null => false
+    t.datetime "updated_at",           :null => false
+  end
+
+  create_table "cheff_cuisine_styles", :force => true do |t|
+    t.integer  "cheff_id"
+    t.integer  "cuisine_style_id"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
   end
 
   create_table "cheff_versions", :force => true do |t|
@@ -60,7 +113,11 @@ ActiveRecord::Schema.define(:version => 20140618065157) do
     t.datetime "cheff_image_updated_at"
     t.string   "hotel_name"
     t.string   "address"
+    t.date     "chef_acquired_on"
+    t.string   "ancestry"
   end
+
+  add_index "cheffs", ["ancestry"], :name => "index_cheffs_on_ancestry"
 
   create_table "cms_attachment_versions", :force => true do |t|
     t.integer  "original_record_id"
@@ -180,6 +237,30 @@ ActiveRecord::Schema.define(:version => 20140618065157) do
     t.boolean  "archived",      :default => false
     t.integer  "created_by_id"
     t.integer  "updated_by_id"
+    t.integer  "food_item_id"
+  end
+
+  create_table "cuisine_geographies", :force => true do |t|
+    t.string   "name"
+    t.string   "ancestry"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+    t.boolean  "deleted"
+    t.boolean  "archived"
+    t.boolean  "published"
+    t.integer  "created_by_id"
+    t.integer  "updated_by_id"
+  end
+
+  create_table "cuisine_styles", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+    t.boolean  "deleted"
+    t.boolean  "archived"
+    t.boolean  "published"
+    t.integer  "created_by_id"
+    t.integer  "updated_by_id"
   end
 
   create_table "dish_versions", :force => true do |t|
@@ -211,38 +292,6 @@ ActiveRecord::Schema.define(:version => 20140618065157) do
     t.string   "category"
     t.string   "cooking_equipment"
     t.string   "treatment"
-  end
-
-  create_table "dishes", :force => true do |t|
-    t.integer  "version"
-    t.integer  "lock_version",            :default => 0
-    t.string   "name"
-    t.boolean  "if_signature",            :default => false
-    t.integer  "portions"
-    t.integer  "days_notice"
-    t.text     "information"
-    t.datetime "created_at",                                 :null => false
-    t.datetime "updated_at",                                 :null => false
-    t.boolean  "published",               :default => false
-    t.boolean  "deleted",                 :default => false
-    t.boolean  "archived",                :default => false
-    t.integer  "created_by_id"
-    t.integer  "updated_by_id"
-    t.integer  "cheff_id"
-    t.string   "dish_image_file_name"
-    t.string   "dish_image_content_type"
-    t.integer  "dish_image_file_size"
-    t.datetime "dish_image_updated_at"
-    t.integer  "price"
-    t.string   "dish_type"
-    t.integer  "ratings",                 :default => 0
-    t.integer  "cooking_time"
-    t.integer  "serves"
-    t.string   "cuisine"
-    t.string   "category"
-    t.string   "cooking_equipment"
-    t.string   "treatment"
-    t.string   "tag_list"
   end
 
   create_table "dynamic_view_versions", :force => true do |t|
@@ -332,6 +381,39 @@ ActiveRecord::Schema.define(:version => 20140618065157) do
   add_index "file_blocks", ["deleted"], :name => "index_file_blocks_on_deleted"
   add_index "file_blocks", ["type"], :name => "index_file_blocks_on_type"
 
+  create_table "food_item_versions", :force => true do |t|
+    t.integer  "original_record_id"
+    t.integer  "version"
+    t.date     "meal_acquired_date"
+    t.datetime "created_at",                            :null => false
+    t.datetime "updated_at",                            :null => false
+    t.string   "name"
+    t.boolean  "published",          :default => false
+    t.boolean  "deleted",            :default => false
+    t.boolean  "archived",           :default => false
+    t.string   "version_comment"
+    t.integer  "created_by_id"
+    t.integer  "updated_by_id"
+  end
+
+  create_table "food_items", :force => true do |t|
+    t.integer  "version"
+    t.integer  "lock_version",       :default => 0
+    t.date     "meal_acquired_date"
+    t.datetime "created_at",                            :null => false
+    t.datetime "updated_at",                            :null => false
+    t.string   "name"
+    t.boolean  "published",          :default => false
+    t.boolean  "deleted",            :default => false
+    t.boolean  "archived",           :default => false
+    t.integer  "created_by_id"
+    t.integer  "updated_by_id"
+    t.integer  "cheff_id"
+    t.boolean  "if_signature",       :default => false
+    t.boolean  "if_recipe",          :default => false
+    t.integer  "dish_served",        :default => 0
+  end
+
   create_table "group_permissions", :force => true do |t|
     t.integer "group_id"
     t.integer "permission_id"
@@ -375,6 +457,21 @@ ActiveRecord::Schema.define(:version => 20140618065157) do
   add_index "groups", ["code"], :name => "index_groups_on_code"
   add_index "groups", ["group_type_id"], :name => "index_groups_on_group_type_id"
 
+  create_table "hola_user_addresses", :force => true do |t|
+    t.string   "address"
+    t.integer  "hola_user_id"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+  end
+
+  create_table "hola_users", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "phoneNumber"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
   create_table "html_block_versions", :force => true do |t|
     t.integer  "original_record_id"
     t.integer  "version"
@@ -412,12 +509,13 @@ ActiveRecord::Schema.define(:version => 20140618065157) do
   create_table "ingredients", :force => true do |t|
     t.integer  "dish_id"
     t.string   "ingredient_name"
-    t.integer  "quantity"
+    t.string   "quantity"
     t.string   "special_remarks"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.string   "unit"
-    t.string   "sub_heading"
+    t.integer  "sub_menu_id"
+    t.integer  "recipe_id"
   end
 
   create_table "link_versions", :force => true do |t|
@@ -450,6 +548,33 @@ ActiveRecord::Schema.define(:version => 20140618065157) do
     t.integer  "created_by_id"
     t.integer  "updated_by_id"
     t.integer  "latest_version"
+  end
+
+  create_table "meal_infos", :force => true do |t|
+    t.integer  "food_item_id"
+    t.string   "name"
+    t.text     "description"
+    t.integer  "preorder_time"
+    t.integer  "portion_size"
+    t.integer  "minimum_order_qty"
+    t.integer  "hola_buy_price"
+    t.integer  "hola_sell_price"
+    t.datetime "created_at",        :null => false
+    t.datetime "updated_at",        :null => false
+  end
+
+  create_table "my_favorite_chefs", :force => true do |t|
+    t.integer  "cheff_id"
+    t.integer  "hola_user_id"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+  end
+
+  create_table "my_favorite_recipes", :force => true do |t|
+    t.integer  "food_item_id"
+    t.integer  "hola_user_id"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
   end
 
   create_table "order_versions", :force => true do |t|
@@ -510,20 +635,33 @@ ActiveRecord::Schema.define(:version => 20140618065157) do
 
   create_table "orders", :force => true do |t|
     t.integer  "version"
-    t.integer  "lock_version",  :default => 0
+    t.integer  "lock_version",             :default => 0
     t.string   "phone_no"
     t.text     "address"
     t.integer  "total"
-    t.datetime "date"
+    t.date     "date"
     t.string   "order_status"
-    t.datetime "created_at",                       :null => false
-    t.datetime "updated_at",                       :null => false
+    t.datetime "created_at",                                  :null => false
+    t.datetime "updated_at",                                  :null => false
     t.string   "name"
-    t.boolean  "published",     :default => false
-    t.boolean  "deleted",       :default => false
-    t.boolean  "archived",      :default => false
+    t.boolean  "published",                :default => false
+    t.boolean  "deleted",                  :default => false
+    t.boolean  "archived",                 :default => false
     t.integer  "created_by_id"
     t.integer  "updated_by_id"
+    t.string   "order_type"
+    t.time     "from_time"
+    t.time     "upto_time"
+    t.text     "payment_gateway_response"
+    t.string   "firstName"
+    t.string   "lastName"
+    t.string   "email"
+    t.string   "addressStreet1"
+    t.string   "addressStreet2"
+    t.string   "addressCity"
+    t.string   "addressState"
+    t.string   "addressCountry"
+    t.string   "addressZip"
   end
 
   create_table "page_route_options", :force => true do |t|
@@ -603,6 +741,17 @@ ActiveRecord::Schema.define(:version => 20140618065157) do
     t.datetime "updated_at",  :null => false
   end
 
+  create_table "pictures", :force => true do |t|
+    t.integer  "picturable_id"
+    t.string   "picturable_type"
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+  end
+
   create_table "portlet_attributes", :force => true do |t|
     t.integer "portlet_id"
     t.string  "name"
@@ -627,9 +776,43 @@ ActiveRecord::Schema.define(:version => 20140618065157) do
   create_table "prepration_steps", :force => true do |t|
     t.integer  "dish_id"
     t.text     "steps"
-    t.string   "sub_heading"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.integer  "sub_menu_id"
+    t.integer  "recipe_id"
+  end
+
+  create_table "rails_admin_histories", :force => true do |t|
+    t.text     "message"
+    t.string   "username"
+    t.integer  "item"
+    t.string   "table"
+    t.integer  "month",      :limit => 2
+    t.integer  "year",       :limit => 8
+    t.datetime "created_at",              :null => false
+    t.datetime "updated_at",              :null => false
+  end
+
+  add_index "rails_admin_histories", ["item", "table", "month", "year"], :name => "index_rails_admin_histories"
+
+  create_table "recipes", :force => true do |t|
+    t.integer  "food_item_id"
+    t.text     "garnishing_tips"
+    t.text     "serving_tips"
+    t.text     "storage_instructions"
+    t.string   "shelf_life"
+    t.string   "equipment_required"
+    t.datetime "created_at",                              :null => false
+    t.datetime "updated_at",                              :null => false
+    t.integer  "preparation_time"
+    t.integer  "total_time"
+    t.integer  "servings"
+    t.boolean  "if_signature",         :default => false
+    t.boolean  "if_recipe",            :default => false
+    t.string   "cuisine"
+    t.string   "category"
+    t.string   "treatment"
+    t.string   "dish_type"
   end
 
   create_table "redirects", :force => true do |t|
@@ -640,6 +823,16 @@ ActiveRecord::Schema.define(:version => 20140618065157) do
   end
 
   add_index "redirects", ["from_path"], :name => "index_redirects_on_from_path"
+
+  create_table "reviews", :force => true do |t|
+    t.integer  "food_item_id"
+    t.date     "review_date"
+    t.string   "reviewer"
+    t.text     "review"
+    t.integer  "ratings",      :default => 0
+    t.datetime "created_at",                  :null => false
+    t.datetime "updated_at",                  :null => false
+  end
 
   create_table "section_nodes", :force => true do |t|
     t.string   "node_type"
@@ -670,6 +863,20 @@ ActiveRecord::Schema.define(:version => 20140618065157) do
     t.boolean  "the_default"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "sub_menus", :force => true do |t|
+    t.integer  "dish_id"
+    t.string   "title"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+    t.boolean  "deleted"
+    t.boolean  "archived"
+    t.boolean  "published"
+    t.integer  "created_by_id"
+    t.integer  "updated_by_id"
+    t.string   "name"
+    t.integer  "recipe_id"
   end
 
   create_table "taggings", :force => true do |t|
@@ -706,6 +913,12 @@ ActiveRecord::Schema.define(:version => 20140618065157) do
   add_index "tasks", ["completed_at"], :name => "index_tasks_on_completed_at"
   add_index "tasks", ["page_id"], :name => "index_tasks_on_page_id"
 
+  create_table "temporary_homes", :force => true do |t|
+    t.string   "email"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
   create_table "tips", :force => true do |t|
     t.integer  "dish_id"
     t.text     "tips"
@@ -722,21 +935,21 @@ ActiveRecord::Schema.define(:version => 20140618065157) do
   add_index "user_group_memberships", ["user_id"], :name => "index_user_group_memberships_on_user_id"
 
   create_table "users", :force => true do |t|
-    t.string   "login",                     :limit => 40
-    t.string   "first_name",                :limit => 40
-    t.string   "last_name",                 :limit => 40
-    t.string   "email",                     :limit => 40
-    t.string   "crypted_password",          :limit => 40
-    t.string   "salt",                      :limit => 40
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.datetime "expires_at"
-    t.string   "remember_token",            :limit => 40
-    t.datetime "remember_token_expires_at"
-    t.string   "reset_token"
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0,  :null => false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
   end
 
-  add_index "users", ["expires_at"], :name => "index_users_on_expires_at"
-  add_index "users", ["login"], :name => "index_users_on_login", :unique => true
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
