33bf67911cb272ea9dc89fd4da1e9669adc79786
Pravin Mhatre <pravinhmhatre@gmail.com>
2014-08-01 22:11:35 +0530

    Handled nil user in order histories

--- app/controllers/order_histories_controller.rb
+++ app/controllers/order_histories_controller.rb
@@ -1,7 +1,7 @@
 class OrderHistoriesController < ApplicationController
   def index
     hola_user = HolaUser.find_by_phoneNumber(cookies[:user_mobile]) if cookies[:user_mobile].present?
-    ordered_menus = hola_user.orders.map{|order| order.ordered_menus }
+    ordered_menus = hola_user.blank? ? [] : hola_user.orders.map{|order| order.ordered_menus }
     @order_histories = ordered_menus.empty? ? [] : ordered_menus.flatten
   end
 end
