f986c3b0d4a96e6da6bd20a146126c461750442e
Arun Verma <arun.verma@sidusa.com>
2014-08-05 21:17:50 +0530

    Removed cusine form Order histroy Added background images(veg/non-veg) for dishes having no image

--- app/helpers/hola_users_helper.rb
+++ app/helpers/hola_users_helper.rb
@@ -1,10 +1,2 @@
 module HolaUsersHelper
-
-  def dish_type_css(food_item)
-    if food_item.meal_info && food_item.meal_info.picture
-      ""
-    else
-      food_item.recipe.dish_type == "non-veg" ? "non-veg" : "veg"
-    end
-  end
 end
