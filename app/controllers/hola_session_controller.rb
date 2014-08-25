b289c5fb881be49d8c73977bae9e1227445da344
Pravin Mhatre <pravinhmhatre@gmail.com>
2014-08-25 15:56:21 +0530

    Fixing cookies issue

--- app/controllers/hola_session_controller.rb
+++ app/controllers/hola_session_controller.rb
@@ -3,7 +3,8 @@ class HolaSessionController < ApplicationController
     hola_user = HolaUser.create_from_params({ phone_no: params[:mobile],
                                               name: params[:name]
                                             })
-    cookies.signed[:user_mobile] = {value: hola_user.phoneNumber, expires: 5.year.from_now} if hola_user
+    session[:hola_user_id] = hola_user.id
+
     respond_to do |format|
       format.js
     end
