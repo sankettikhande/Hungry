8d4ebcc0c162e4765fb3da1635721a58a2ce723d
Pravin Mhatre <pravinhmhatre@gmail.com>
2014-10-01 17:22:53 +0530

    Changed messaging to smsgupshup api

--- lib/messaging_lib.rb
+++ lib/messaging_lib.rb
@@ -4,15 +4,10 @@ module MessagingLib
 	class << self
 		def send_messages(message,numbers,message_type)
 			encode_msg = message.split(' ').join('+')
-			url = Settings.messaging_default_url + "&to=#{numbers}&message=#{encode_msg}"
+			url = Settings.messaging_default_url + "&send_to=#{numbers}&msg=#{encode_msg}"
 			result = Net::HTTP.get(URI.parse(url))
-			id = (result.include?("Message") ? result.split(" ID=").last : nil)
-			message = MessageReport.create(:numbers => numbers, :message_type => message_type, :message => message, :message_id => id, :status => result)
-			if id
-				retrace_url = Settings.messaging_default_retrace_url + "&messageid=#{id}"
-				status = Net::HTTP.get(URI.parse(retrace_url)).split("\n").last
-				message.update_attribute(:status, status)
-			end
+			id = (result.include?("success") ? result.split("|").last.strip : nil)
+			MessageReport.create(:numbers => numbers, :message_type => message_type, :message => message, :message_id => id, :status => result)
 		end
 	end
 end
