a8ba0ad252056da2e16c243f751a5a54ea3a6171
suvarna <srkanade.08@gmail.com>
2014-10-07 17:33:25 +0530

    coupon code changes

--- app/controllers/ordered_menus_controller.rb
+++ app/controllers/ordered_menus_controller.rb
@@ -16,6 +16,32 @@ class OrderedMenusController < ApplicationController
 
   end
 
+  def check_coupon_code
+    if !params[:coupon_code].blank? && !params[:total_amount].blank?
+      coupon = Coupon.find_by_coupon_code(params[:coupon_code])
+      if coupon.blank?
+        @msg = "Wrong coupon code"
+      else
+        ## check (coupon.no_of_coupons == coupon.no_of_used_coupons)
+        discount = coupon.flat.blank? ? coupon.percentage : coupon.flat
+        discount_type = coupon.flat.blank? ? "percentage" : "flat"
+        if !discount.blank?
+          if discount_type== "flat"
+            @discount_amount = discount
+          else
+            @discount_amount = Coupon.calculate_percentage(params[:total_amount],discount)
+          end
+          @paid_amount = params[:total_amount].to_i -  @discount_amount.to_i
+          @msg = "Valid coupon code"
+        end
+      end
+      respond_to do |format|
+        format.js
+      end
+    end
+
+  end
+
   def get_address_by_type
     hola_user = hola_current_user
     @adds = hola_user.hola_user_addresses.where(address_type: params[:address_type]).first
