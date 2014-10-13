18fbb593672a487c63c1f0d8b86b76c27b072805
suvarna <srkanade.08@gmail.com>
2014-10-13 15:49:03 +0530

    discount coupons changes

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -11,4 +11,18 @@ class Coupon < ActiveRecord::Base
     amount = ((per.to_f / 100) * amount.to_i).to_i
     return amount
   end
+
+  def check_coupons_validity(user_id)
+      coupon = Coupon.find_by_coupon_code(self.coupon_code)
+      if !coupon.blank?
+        if (coupon.no_of_coupons == coupon.no_of_used_coupons)
+          return "You can not use this coupon. validity Reached"
+        else
+          if coupon.coupon_type == '1U1T' && !coupon.hola_users.blank?
+           user_coupon = coupon.hola_users.where(:id =>  user_id)
+           return user_coupon.blank? ? "" : "#{self.coupon_code} coupon already used."
+          end
+        end
+      end
+  end
 end
