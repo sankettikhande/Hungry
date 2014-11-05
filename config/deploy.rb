d104240b53172125095221b854f4ff4a5ab37f05
Rahul Agarwal <rahul.agarwal@gmail.com>
2014-11-05 18:03:18 +0530

    changes for newrelic.

--- config/deploy.rb
+++ config/deploy.rb
@@ -82,7 +82,7 @@ namespace :deploy do
   end
 end
 
-after "deploy:update_code", "db:symlink"
+after "deploy:update_code", "db:symlink", "newrelic:symlink"
 after "deploy:update", "deploy:cleanup"
 
 after "deploy:cleanup", "deploy:switch_backoffice"
