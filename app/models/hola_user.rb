3d858127ce46902c1196b1fcf0eddf74d52a1738
suvarna <srkanade.08@gmail.com>
2014-11-11 19:54:27 +0530

    cleanup code

--- app/models/hola_user.rb
+++ app/models/hola_user.rb
@@ -60,7 +60,7 @@ class HolaUser < ActiveRecord::Base
 
   def self.confirm_orders_last_3_days(hola_user_id)
     hola_user = HolaUser.find_by_id(hola_user_id)
-    return if hola_user ? hola_user.orders.confirm_orders.where("date >= ? and date <= ?",3.days.ago, Date.today).count : 0
+    return  !hola_user.blank? ? hola_user.orders.confirm_orders.where("date >= ? and date <= ?",3.days.ago, Date.today).count : 0
   end
 
 end
