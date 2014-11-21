bf5e8ef2cb442b6903331d2550e3f5578b9751fc
Pravin Mhatre <pravinhmhatre@gmail.com>
2014-11-21 13:38:41 +0530

    Fixed migration to set password

--- db/migrate/20141120111112_add_user_name_and_password_to_existing_runner.rb
+++ db/migrate/20141120111112_add_user_name_and_password_to_existing_runner.rb
@@ -3,7 +3,7 @@ class AddUserNameAndPasswordToExistingRunner < ActiveRecord::Migration
     Runner.all.each do |runner|
       if runner.username.blank? && runner.password.blank?
         runner.username = runner.name.downcase.gsub(" ", "_")+"_"+runner.id.to_s
-        runner.password = Base64.encode64(runner.name+"_"+runner.id.to_s)
+        runner.password = runner.username
         runner.save
       end
     end
