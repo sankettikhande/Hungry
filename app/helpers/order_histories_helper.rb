837cdcc38277d4596d583b84d3b9a57fbd6f140a
pradnyakulkarni <pradnya@sodelsolutions.com>
2014-11-26 10:47:29 +0530

    redefine code

--- app/helpers/order_histories_helper.rb
+++ app/helpers/order_histories_helper.rb
@@ -1,2 +1,15 @@
 module OrderHistoriesHelper
+	# Added for getting ratings on order history page
+  	# On 26/11/2014 By Pradnya Kulkarni
+  	# Contact: pradnya@sodelsolutions.com
+  	def get_ratings(order_id, food_item_id)
+    	review = Review.find_by_hola_user_id_and_order_id_and_food_item_id(hola_current_user, order_id, food_item_id)
+    	if review.present?
+      		ratings = review.try(:ratings)
+    	else
+      		ratings = ''
+    	end
+    	return ratings
+    end
+
 end
