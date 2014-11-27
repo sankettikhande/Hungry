3a7ebb9503afcd02af7d3e8ea66564769b960e20
anil <anil.gelra@gmail.com>
2014-11-27 18:10:35 +0530

    Changes by deepak

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -40,17 +40,17 @@ $(document).ready(function(){
         $(".undefinedformError").css('left','21px');
     })
     $("#radio-nbanking").click(function(){
-       $(".netbanking").show();
-       $(".cash_delivery").hide();
-       $(".select_payment_method").hide();
-       $(".undefinedformError").hide();
+       $(".netbanking").css("display","inline-block");
+       $(".cash_delivery").css("display","none");
+       $(".select_payment_method").css("display","none");
+       $(".undefinedformError").css("display","none");
     });
 
     $("#radio-cod").click(function(){
-       $(".cash_delivery").show();
-       $(".netbanking").hide();
-       $(".select_payment_method").hide();
-       $(".undefinedformError").hide();
+       $(".cash_delivery").css("display","inline-block");
+       $(".netbanking").css("display","none");
+       $(".select_payment_method").css("display","none");
+       $(".undefinedformError").css("display","none");
     });
 
     $('#add-order-plus').unbind("click").live('click', function(e) {
