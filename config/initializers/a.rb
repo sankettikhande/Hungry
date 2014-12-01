b8e5b5b5c64bfcd3a909a9fa86ac67cac12e6d3d
Pravin Mhatre <pravinhmhatre@gmail.com>
2014-12-01 15:00:14 +0530

    Fixed all time meal issue Delete issue

--- config/initializers/a.rb
+++ config/initializers/a.rb
@@ -3,3 +3,26 @@ Cms::DynamicView.class_eval do
     File.join(Rails.root, "uploads")
   end
 end
+
+
+
+#BCMS delete override
+module Cms
+  module Behaviors
+    module SoftDeleting
+      module InstanceMethods
+        def destroy
+          run_callbacks :destroy do
+            if self.class.publishable?
+              self.deleted = true
+              self.publish_on_save = true
+              self.save(validate: false)
+            else
+              update_attribute(:deleted, true)
+            end
+          end
+        end
+      end
+    end
+  end
+end
