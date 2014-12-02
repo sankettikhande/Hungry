7342f6410a5f9bb544aa4b13325e6cfdd195a9d7
anil <anil.gelra@gmail.com>
2014-12-02 18:21:42 +0530

    UI Changes from deepak - Fly to cart

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -74,6 +74,45 @@ $(document).ready(function(){
             $('#login_otp_sms_button').attr("disabled", true)
         }
     })
+    //  working on Fly to Cart effect
+    $('.increase-order-item').on('click', function () {
+            var cart = $('.bag-count');
+            var imgtodrag = $(this).parents('.square').find(".imageItemFix").eq(0);
+            if (imgtodrag) {
+                var imgclone = imgtodrag.clone()
+                    .offset({
+                    top: imgtodrag.offset().top,
+                    left: imgtodrag.offset().left
+                })
+                    .css({
+                    'opacity': '0.5',
+                        'position': 'absolute',
+                        'height': '150px',
+                        'width': '150px',
+                        'z-index': '9999'
+                })
+                .appendTo($('body'))
+                .animate({
+                    'top': cart.offset().top + 10,
+                        'left': cart.offset().left + 10,
+                        'width': 75,
+                        'height': 75
+                }, 1000, 'easeInOutExpo');
+
+                setTimeout(function () {
+                    cart.effect("shake", {
+                        times: 2
+                    }, 200);
+                }, 1500);
+
+                imgclone.animate({
+                    'width': 0,
+                        'height': 0
+                }, function () {
+                    $(this).detach()
+                });
+            }
+        });
 
     $('.add-order-plus').unbind("click").live('click', function(e) {
         e.preventDefault();
