99b69470d28fc4f868c0f19df1538c7f4b2f1808
Pravin Mhatre <pravinhmhatre@gmail.com>
2014-12-09 18:09:05 +0530

    Backoffice for api version2

---
+++ public/backoffice2/app/store/OrderStore.js
@@ -0,0 +1,16 @@
+﻿Ext.define('HolaChef.store.OrderStore', {
+    extend: 'Ext.data.Store',
+    config: {
+        model: 'HolaChef.model.OrderModel',
+        proxy: {
+            type: 'ajax',
+            url: 'data/orders.json',
+            reader: {
+                type: 'json',            
+            },            
+            headers: {
+                'Accept': 'application/json'
+            }
+        } 
+    }
+});
---
+++ public/backoffice2/app/view/ManageRunner.js
@@ -0,0 +1,20 @@
+﻿Ext.define('HolaChef.view.ManageRunner', {
+    extend: 'Ext.Container',
+    xtype: 'ManageRunner',
+
+    requires: [
+        'Ext.Label',
+        'Ext.Container',
+        'Ext.Button'
+    ],
+
+    config:
+        {
+            items: [{
+                xtype: 'container',
+                id: 'ManageRunnerHtmlID',
+                html: ''
+            }]
+
+        }
+});
