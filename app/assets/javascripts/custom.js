e24e2cb9a00d86be722ddde99e0487903c57a219
Rahul Agarwal <rahul.agarwal@gmail.com>
2014-12-18 13:36:39 +0530

    Revert "Revert "Staging""
    This reverts commit 734440016385cf5191e791dcd6e8da7ea34c43a5.
    
    Conflicts:
    	config/deploy.rb

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -58,6 +58,9 @@ $(document).ready(function(){
         $("#add-order-minus").removeClass("disabled");
         $("#add-order-minus").removeAttr("disabled");
         var max_qty = parseInt($("#item_max_qty").val(), 10);
+        
+        cnt = parseInt($("#order-count").html(),10);
+        
         if (cnt < parseInt(max_qty,10)){
             cnt = parseInt($("#order-count").html(),10) + 1;
             $("#order-count").html(cnt);
@@ -622,7 +625,7 @@ $(document).ready(function(){
                 $.ajax({
                   'url' : '/submit_payment_form',
                   'method': 'POST',
-                  'data': {'paymentMode': payment_mode, 'orderId':order_id},
+                  'data': {'paymentMode': payment_mode, 'orderId':order_id, 'authenticity_token': $("#payment_form_atk").html()},
                   'dataType':'script'
                 });
                 
--- app/controllers/api_v2/orders_controller.rb
+++ app/controllers/api_v2/orders_controller.rb
@@ -1,4 +1,4 @@
-class ApiV2::OrdersController < ApplicationController
+class ApiV2::OrdersController < ApiController
   def index
     @orders = Order.order_items_list(params[:from_date], params[:to_date])
   end
